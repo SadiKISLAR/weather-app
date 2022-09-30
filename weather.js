e5282b47448a1ccc231ec86565a84b36e21015a2
SadiKISLAR <sadi22kislar@gmail.com>
2022-09-30 10:04:50 +0300

    değişkenler bitti

---
+++ weather.js
@@ -0,0 +1,4 @@
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector("input");
+const msg = document.querySelector("span.msg");
+const list = document.querySelector("ajax-section ul.cities");
